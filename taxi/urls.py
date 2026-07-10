fddc46f18efb313c58037fb7426c711012a73557
Sardor <sardor.tozhiev1994@gmail.com>
2026-07-10 20:09:45 +0300

    work is done

--- taxi/urls.py
+++ taxi/urls.py
@@ -34,12 +34,11 @@ urlpatterns = [
          name="manufacturer-delete"
          ),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/create/", CarCreateView.as_view(), name="cars-create"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="cars-update"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="cars-delete"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
